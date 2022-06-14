661db37dca5a3c7a40ba24a68e4ee9f9aa27d469
Arcohol <i@arcohol.com>
2022-06-14 11:44:16 +0800

    lab7

---
+++ src/labs/lab7/CatchButtonGameV1.java
@@ -0,0 +1,65 @@
+package labs.lab7;
+
+import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class CatchButtonGameV1 {
+    JFrame frame;
+    JButton[] buttons;
+    int index;
+
+    public CatchButtonGameV1() {
+        frame = new JFrame("Catch me if you can!");
+        buttons = new JButton[9];
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(3, 3));
+        frame.setSize(600, 600);
+        frame.setVisible(true);
+
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i] = new JButton();
+            frame.add(buttons[i]);
+        }
+
+        MouseListener l = new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                JButton b = (JButton) e.getSource();
+                b.setText("");
+                b.removeMouseListener(this);
+                index = (index + (int) (Math.random() * buttons.length + 1)) % buttons.length;
+                buttons[index].addMouseListener(this);
+                buttons[index].setText("Click me");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
+        };
+
+        index = (index + (int) (Math.random() * buttons.length + 1)) % buttons.length;
+        buttons[index].addMouseListener(l);
+        buttons[index].setText("Click me");
+    }
+
+    public static void main(String[] args) {
+        CatchButtonGameV1 game = new CatchButtonGameV1();
+    }
+}
